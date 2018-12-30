19bb6971f9ccad1c9f6398b14f145a0ddf62e403
Patil, Saurabh <PatilS@advisory.com>
2018-12-30 18:07:02 -0500

    Using event handling and component state

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Saurabh', age='29'},
-      {name: 'Ripinka', age='27'}
+      {name: 'Saurabh', age: '29', title: 'Software Engineer'},
+      {name: 'Ripinka', age: '27'}
     ]
+  }
+
+  nameChangeHandler = (lastName) => {
+    this.setState({
+      persons: [
+        {name: 'Saurabh ' + lastName, age: '29', title: 'Software Engineer'},
+        {name: 'Ripinka ' + lastName, age: '27'}
+      ]
+    })
   }
 
   render() {
@@ -17,8 +26,10 @@ class App extends Component {
           <p>
             React Tutorial
           </p>
-          <Person name='Saurabh' age='29'>Job Title: Software Engineer</Person>
-          <Person name='Ripinka' age='27'></Person>
+          <button className="App-btn" onClick={this.nameChangeHandler.bind(this, 'Patil')}>Show FullName</button>
+          {this.state.persons.map(person => (
+            <Person name={person.name} age={person.age}>{person.title ? 'Job Title: ' + person.title : ''}</Person>
+          ))}
         </header>
       </div>
     );
