f6613b43f7f052da5cec59ac2206c7cfee1652e8
Patil, Saurabh <PatilS@advisory.com>
2019-05-09 13:42:21 -0400

    Adding routing in react tutorial

---
+++ react-routing/src/containers/Blog/Blog.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+// import axios from 'axios';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import './NewPost/NewPost';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <Posts />} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
